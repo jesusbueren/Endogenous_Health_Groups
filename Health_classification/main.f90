f213d688ba2ec93cd60e05e0afcdf761ccf5d457
Jesus Bueren <76816117+jesusbueren@users.noreply.github.com>
2021-04-06 16:28:03 +0200

    Update main.f90

--- Health_classification/main.f90
+++ Health_classification/main.f90
@@ -28,43 +28,7 @@ program main
     !Initial guess for the smoothed states
     smoothed_states=1.0d0/dble(clusters)
     
-    !print*,'Compute smoothed probability/full info for econometritian'
-    !call likelihood_all(p_star,likelihood)
-    !do it=1,100 !I run it 100 so that initial conditions for filter don't matter
-    !    print*,it
-    !    call filtration(H,p_star,smoothed_states,likelihood,filtered_states)
-    !    call smoothing(filtered_states,H,p_star,sample_k,smoothed_states)  
-    !end do
-    !
-    !open(unit=9,file=path//path_s2//'smoothed_pr_'//s_c//'.txt',status='replace')
-    !do i_l=1,indv; do g_l=1,generations;
-    !    if (clusters==2) then
-    !       ! write(9,'(I5,I3,F6.3,F6.3,I3)'), i_l,g_l,smoothed_states(i_l,1,g_l),smoothed_states(i_l,2,g_l),maxloc(smoothed_states(i_l,:,g_l))
-    !    elseif (clusters==3) then
-    !        !write(9,'(I5,I3,F6.3,F6.3,F6.3,I3)'), i_l,g_l,smoothed_states(i_l,1,g_l),smoothed_states(i_l,2,g_l),smoothed_states(i_l,3,g_l),maxloc(smoothed_states(i_l,:,g_l))
-    !    elseif (clusters==4) then
-    !        write(9,'(I5,I3,F6.3,F6.3,F6.3,F6.3,I3)'), i_l,g_l,smoothed_states(i_l,1,g_l),smoothed_states(i_l,2,g_l),smoothed_states(i_l,3,g_l),smoothed_states(i_l,4,g_l),maxloc(smoothed_states(i_l,:,g_l))
-    !    elseif (clusters==5) then
-    !        !write(9,'(I5,I3,F6.3,F6.3,F6.3,F6.3,F6.3,I3)'), i_l,g_l,smoothed_states(i_l,1,g_l),smoothed_states(i_l,2,g_l),smoothed_states(i_l,3,g_l),smoothed_states(i_l,4,g_l),smoothed_states(i_l,5,g_l),maxloc(smoothed_states(i_l,:,g_l))
-    !    end if
-    !end do;end do
-    !close(9)
-    !
-    !!Compute average absolute deviation between filter and smoother
-    !counter=0.0d0
-    !chg=0.0d0
-    !do i_l=1,indv; do g_l=1,generations;
-    !    if (sample_k(i_l,g_l)/=-1 .and. sample_k(i_l,g_l)/=clusters+1) then
-    !        if (maxloc(smoothed_states(i_l,:,g_l),1)/=maxloc(filtered_states(i_l,:,g_l),1)) then
-    !            chg=chg+1
-    !        end if
-    !        counter=counter+1.0d0
-    !    end if
-    !end do;end do
-    !
-    !print*,'Percent of people that change status from filter to smoother'
-    !print*,chg/counter*100
-    
+      
     print*,'Compute filtered probability for the agent: assume that before seeing his health, he has the av health of his education and sex'
     
     call load_init_cond(H,smoothed_states)
@@ -73,13 +37,16 @@ program main
     
     open(unit=9,file=path//path_s2//'naive_pr_'//s_c//'.txt',status='replace')
     do i_l=1,indv; do g_l=1,generations;
-        if (clusters==2) then
+            !With two clusters uncomment the following line, comment otherwise
             !write(9,'(I5,I3,F6.3,F6.3,I3)'), i_l,g_l,filtered_states(i_l,1,g_l),filtered_states(i_l,2,g_l),maxloc(filtered_states(i_l,:,g_l))
-        elseif (clusters==3) then
+            
+            !With three clusters uncomment the following line, comment otherwise
             !write(9,'(I5,I3,F6.3,F6.3,F6.3,I3)'), i_l,g_l,filtered_states(i_l,1,g_l),filtered_states(i_l,2,g_l),filtered_states(i_l,3,g_l),maxloc(filtered_states(i_l,:,g_l))
-        elseif (clusters==4) then
+            
+            !With four clusters uncomment the following line, comment otherwise
             write(9,'(I5,I3,F6.3,F6.3,F6.3,F6.3,I3)'), i_l,g_l,filtered_states(i_l,1,g_l),filtered_states(i_l,2,g_l),filtered_states(i_l,3,g_l),filtered_states(i_l,4,g_l),maxloc(filtered_states(i_l,:,g_l))
-        elseif (clusters==5) then
+            
+            !With five clusters uncomment the following line, comment otherwise
             !write(9,'(I5,I3,F6.3,F6.3,F6.3,F6.3,F6.3,I3)'), i_l,g_l,filtered_states(i_l,1,g_l),filtered_states(i_l,2,g_l),filtered_states(i_l,3,g_l),filtered_states(i_l,4,g_l),filtered_states(i_l,5,g_l),maxloc(filtered_states(i_l,:,g_l))
         end if
     end do;end do
